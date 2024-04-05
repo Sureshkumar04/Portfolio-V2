fa015f22c3bb2416e0d3c45efb08a92e64bee4c9
Sureshkumar04 <112899987+Sureshkumar04@users.noreply.github.com>
2024-04-05 21:16:52 +0530

    animationone

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
 Fancybox.bind("[data-fancybox]", {
 });
 
-AOS.init();  
+
+
+// matte title
+
+const fadeup = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("fade-up-show");
+        }else{
+            entry.target.classList.remove("fade-up-show");
+
+        }
+    });
+});   
+
+const fadeuphidden = document.querySelectorAll(".fade-up-hidden");
+fadeuphidden.forEach((el)=>fadeup.observe(el));
+
+// fade left
+
+const fadeLeft = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("fade-left-show");
+        }else{
+            entry.target.classList.remove("fade-left-show");
+
+        }
+    });
+});   
+
+const fadelefthidden = document.querySelectorAll(".fade-left-hidden");
+fadelefthidden.forEach((el)=>fadeLeft.observe(el));
+
+
+// Fade down
+
+const fadedown = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("fade-down-show");
+        }else{
+            entry.target.classList.remove("fade-down-show");
+
+        }
+    });
+});   
+
+const fadedownhidden = document.querySelectorAll(".fade-down-hidden");
+fadedownhidden.forEach((el)=>fadedown.observe(el));
